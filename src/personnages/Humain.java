332dfc6d1ba955bd76a3f11cc5cda1294d3e5fc6
Damien Bonzom <bnd4424a@ens.fsi>
2023-03-28 16:22:34 +0200

    création de la classe Humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,50 @@
+package personnages;
+
+public class Humain {
+	private String name;
+	private String favDrink;
+	private int money;
+	
+	public Humain(String nom, String boisson, int argent) {
+		this.name = nom;
+		this.favDrink = boisson;
+		this.money = argent;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public int getMoney() {
+		return money;
+	}
+	
+	private void parler(String texte) {
+		System.out.println("("+this.name+") - " + texte);
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour ! Je m'appelle " + this.name + " et j'aime boire du " + this.favDrink +".");	
+	}
+	
+	public void boire() {
+		parler("Mmmm, un bon verre de " + this.favDrink + " ! GLOUPS !");
+	}
+	
+	public void acheter(String bien, int prix) {
+		if(this.money >= prix) {
+			parler("J'ai " + this.money + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous");
+			perdreArgent(prix);
+		}else {
+			parler("Je n'ai plus que " + this.money + " sous en poche. Je ne peux même pas m'offrir " + bien + " à " + prix + " sous.");
+		}
+	}
+	
+	public void gagnerArgent(int argent) {
+		this.money += argent;
+	}
+	
+	public void perdreArgent(int argent) {
+		this.money -= argent;
+	}
+
+}
